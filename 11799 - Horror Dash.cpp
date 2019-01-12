862517cd55a7ff670550c8bd0721617664cb8131
S_Way <s.r.shuchi@gmail.com>
2019-01-12 21:22:56 +0600

    Add files via upload

---
+++ 11799 - Horror Dash.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+     int testCase, n, i = 1, j;
+     int m1, m2, speed;
+
+     scanf("%d", &testCase);
+
+     while(i <= testCase)
+     {
+         scanf("%d", &n);
+         scanf("%d", &m1);
+
+         if(n == 1)
+         {
+             speed = m1;
+         }
+         for(j = 1; j < n; j++)
+         {
+             scanf("%d", &m2);
+
+             if(m2 > m1)
+             {
+                 speed = m2;
+             }
+             else
+             {
+                 speed = m1;
+             }
+             m1 = speed;
+         }
+         printf("Case %d: %d\n", i, speed);
+         i++;
+     }
+     return 0;
+}
