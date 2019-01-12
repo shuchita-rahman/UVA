fa746f44dd1cfec7ac8acdd834d31d6c3ea7888a
S_Way <s.r.shuchi@gmail.com>
2019-01-12 21:22:10 +0600

    Add files via upload

---
+++ 11192 - Group Reverse.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int n, i, j, r = 1, len;
+    char input[200];
+
+    while(scanf("%d", &n) == 1 && n != 0)
+    {
+        scanf("%s", input);
+
+        len = strlen(input);
+        n = len / n;
+
+        for(i = 0; input[i] != '\0'; i++)
+        {
+             if(r == n)
+             {
+                 for(j = i; r >= 1; j--, r--)
+                 {
+                     if(input[i + 1] == '\0' && r == 1)
+                     {
+                        printf("%c\n", input[j]);
+                     }
+                     else
+                     {
+                         printf("%c", input[j]);
+                     }
+                 }
+             }
+             r++;
+        }
+    }
+    return 0;
+}
+
