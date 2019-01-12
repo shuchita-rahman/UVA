c9f6f3232c57b2e7fef3cc02383712b50dc66e88
S_Way <s.r.shuchi@gmail.com>
2019-01-12 21:07:09 +0600

    Add files via upload

---
+++ 484 - The Department of Redundancy Department.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    long long int i = 0, len = -1, j, x, flag = 0;
+    long long int a[50000], b[50000];
+
+    while(scanf("%d", &x) == 1)
+    {
+        for(i = 0; i <= len; i++)
+        {
+            if(x == a[i])
+            {
+               flag = 1;
+               b[i] = b[i] + 1;
+               break;
+            }
+        }
+        if(flag == 0)
+        {
+            len = len + 1;
+            a[len] = x;
+        }
+    }
+
+    return 0;
+}
+
